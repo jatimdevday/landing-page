cc5d92f48dc148bf00dc3025cef8657adf793e04
wildan <wildanrgsspeed@gmail.com>
2020-07-10 14:49:06 +0700

    fixing text main

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,12 @@
     <NavBar />
     <div class="main">
       <h1 class="title">
-        wakakakaka
+        Jatim Developer Day
       </h1>
+      <h2 class="subtitle">
+        Kolaborasi antar komunitas di Jawa Timur yang berawal dari guyonan <br>
+        di salah satu grup diskusi komunitas
+      </h2>
     </div>
   </div>
 </template>
